a3c4e6a7608915ff6f6202a2b36441130df07823
y0mir <cronork@gmail.com>
2017-07-28 11:22:51 -0700

    Skipping refactoring garbage cause it tilts the shit out of me

---
+++ Maze with Friends/Maze with Friends/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Maze with Friends
+//
+//  Created by Ethan on 7/28/17.
+//  Copyright © 2017 Ethan. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct Segue {
+        static let toCreateUsername = "toCreateUsername"
+    }
+}
--- Maze with Friends/Maze with Friends/LoginViewController.swift
+++ Maze with Friends/Maze with Friends/LoginViewController.swift
@@ -7,9 +7,25 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+import FirebaseDatabase
+
+typealias FIRUser = FirebaseAuth.User
+
 
 class LoginViewController: UIViewController {
 
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,15 +39,71 @@ class LoginViewController: UIViewController {
     
 
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        print("login button tapped")
+        // 1
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else { return }
+        
+        // 2
+        authUI.delegate = self
+        
+        // 3
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
     }
-    */
+    
 
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+           // assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        
+        // 1
+        guard let user = user
+            else { return }
+        
+        // 2
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        
+        // 3
+        
+        userRef.observeSingleEvent(of: .value, with: { [unowned self] (snapshot) in
+            if let user = User(snapshot: snapshot) {
+                User.setCurrent(user)
+                
+                let storyboard = UIStoryboard(name: "Main", bundle: .main)
+                if let initialViewController = storyboard.instantiateInitialViewController() {
+                    self.view.window?.rootViewController = initialViewController
+                    self.view.window?.makeKeyAndVisible()
+                }
+            } else {
+                self.performSegue(withIdentifier: "toCreateUsername", sender: self)
+            }
+        })
+        
+        
+        
+//        UserService.show(forUID: user.uid) { (user) in
+//            if let user = user {
+//                // handle existing user
+//                User.setCurrent(user)
+//                
+//                let storyboard = UIStoryboard(name: "Main", bundle: .main)
+//                if let initialViewController = storyboard.instantiateInitialViewController() {
+//                    self.view.window?.rootViewController = initialViewController
+//                    self.view.window?.makeKeyAndVisible()
+//                }
+//            } else {
+//                // handle new user
+//                self.performSegue(withIdentifier: "toCreateUsername", sender: self)
+//            }
+//        }
+        
+        
+    }
 }
